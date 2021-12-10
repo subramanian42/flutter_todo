104bfbceb90649c0748f5fde2ee065c306c0776a
subramanian42 <88108372+subramanian42@users.noreply.github.com>
2021-12-10 22:51:13 +0530

    code refactoring

--- lib/business/authentication.dart
+++ lib/business/authentication.dart
@@ -1,12 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-//import 'package:firebase_core/firebase_core.dart';
-//import 'package:flutter/material.dart';
-
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:todolist_1/pages/homepage.dart';
-//import 'package:todolist_1/pages/homepage.dart';
 
 class Authentication {
   final GoogleSignIn googleSignIn = GoogleSignIn();
